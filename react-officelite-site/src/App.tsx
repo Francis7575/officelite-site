b5872b2112be959ed05a8b4103024a57e23798e4
Francis7575 <francis.albert.2009@gmail.com>
2024-05-03 15:42:05 -0700

    updated

--- react-officelite-site/src/App.tsx
+++ react-officelite-site/src/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/officelite-site" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
   )
